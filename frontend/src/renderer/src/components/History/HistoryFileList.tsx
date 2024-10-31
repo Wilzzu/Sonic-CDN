8de95f67fdff6ad3fe770d158dde51abc15ab9ef
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2024-11-01 01:24:52 +0200

    Add example history item for test release build

--- frontend/src/renderer/src/components/History/HistoryFileList.tsx
+++ frontend/src/renderer/src/components/History/HistoryFileList.tsx
@@ -7,6 +7,12 @@ type HistoryFileListProps = {
   updateStorageSpace: () => Promise<void>
 }
 
+// const exampleFile = {
+//   name: 'example-file.png',
+//   url: 'http://localhost:3001/example-file.png',
+//   size: 1000000
+// }
+
 const HistoryFileList: FC<HistoryFileListProps> = ({ files, updateStorageSpace }): JSX.Element => {
   if (files.length === 0) return <p className="pt-2">No files found in history</p>
 
@@ -14,6 +20,7 @@ const HistoryFileList: FC<HistoryFileListProps> = ({ files, updateStorageSpace }
     <div className="w-full px-3">
       {/* Scroll container */}
       <ul className="w-full h-[calc(100dvh-285px)] px-2 flex flex-col gap-1 overflow-y-auto scrollbar scrollbar-w-[6px] scrollbar-track-transparent scrollbar-thumb-secondary scrollbar-thumb-rounded-full">
+        {/* <HistoryItem file={exampleFile} index={-1} updateStorageSpace={updateStorageSpace} /> */}
         {[...files].reverse().map((file, index) => {
           return (
             <li key={file.name}>
